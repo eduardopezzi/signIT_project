7b58a500d626ec211a344a1f68f0cef12d9d6bb9
eduardopezzi <eduardopezzi@me.com>
2020-04-24 15:39:44 -0400

    upload deck

--- signIT-React-App/README.md
+++
@@ -1,8 +0,0 @@
-# Media-Time-Stamp-React
-
-
-1. Clone this repo
-
-2. Npm install 
-
-3. Npm Start 
--- signIT-React-App/truffle/migrations/2_deploy_contract.js
+++
@@ -1,5 +0,0 @@
-const MediaTimeStamp = artifacts.require("MediaTimeStamp");
-
-module.exports = function(deployer) {
-  deployer.deploy(MediaTimeStamp);
-};
